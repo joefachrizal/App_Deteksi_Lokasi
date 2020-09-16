ebe0190ab705db7c7e4a7bfaa6765a6b892fb7ad
joe_fachrizal <joefachrizal@gmail.com>
2020-09-16 14:19:31 +0700

    mengaktifkan Hak akses pada halaman MainActivity

--- app/src/main/java/com/example/appdeteksilokasi/MainActivity.kt
+++ app/src/main/java/com/example/appdeteksilokasi/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.appdeteksilokasi
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.appdeteksilokasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val ID_LOCATION_PERMISSION = 0
+    }
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkPermissionLocation()
         onClick()
     }
+
 
     private fun onClick() {
         binding.fabCheckIn.setOnClickListener {
@@ -40,5 +54,75 @@ class MainActivity : AppCompatActivity() {
             tvScanning.visibility = View.GONE
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ID_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED ||
+                grantResults[1] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(this, "Akses diizinkan", Toast.LENGTH_SHORT).show()
+                if (!isLoactionEnable()) {
+                    Toast.makeText(this, "Silahakan Aktifkan Lokasi Kamu", Toast.LENGTH_SHORT)
+                        .show()
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            } else {
+                Toast.makeText(this, "Akses Tidak diizinkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkPermissionLocation() {
+        if (checkPermission()) {
+            if (!isLoactionEnable()) {
+                Toast.makeText(this, "Silahakan Aktifkan Lokasi Kamu", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+        } else {
+            reqPermission()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun isLoactionEnable(): Boolean {
+        val locManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun reqPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), ID_LOCATION_PERMISSION
+        )
+    }
+
 
 }
